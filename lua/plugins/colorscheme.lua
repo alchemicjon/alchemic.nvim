4e3424c8facd275f64ca6aaed15243ddffebe028
alchemicjon <jrichmond124@gmail.com>
2026-06-24 10:20:31 -0400

    Remove commented code, update default colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,3 @@
--- return {
---   "catppuccin/nvim",
---   name = "catppuccin",
---   lazy = false,
---   priority = 1000,
---   opts = {
---     show_end_of_buffer = true,
---     flavour = "macchiato",
---   },
---   config = function(_, opts)
---     require("catppuccin").setup(opts)
---     vim.cmd([[colorscheme catppuccin]])
---   end
--- }
-
 return {
 	"sainnhe/everforest",
 	lazy = false,
