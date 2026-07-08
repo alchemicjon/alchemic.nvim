9e2722015f1aef71b66b0101499840c03973ca82
alchemicjon <jrichmond124@gmail.com>
2026-07-08 10:27:11 -0400

    Update load-order of configs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,6 +1,3 @@
-require("config.vim-options")
-require("config.keymaps")
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
