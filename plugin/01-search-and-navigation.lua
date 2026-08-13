b4759fc9b9655023baa9942f551761377c731a2f
alchemicjon <jrichmond124@gmail.com>
2026-08-13 13:46:54 -0400

    Make sure fzf extension loads and runs for telescope

--- plugin/01-search-and-navigation.lua
+++ plugin/01-search-and-navigation.lua
@@ -70,8 +70,8 @@ do
   }
 
   -- Enable Telescope extensions if they are installed
-  pcall(require('telescope').load_extension, 'fzf')
-  pcall(require('telescope').load_extension, 'ui-select')
+  require('telescope').load_extension('fzf')
+  require('telescope').load_extension('ui-select')
 
   -- See `:help telescope.builtin`
   local builtin = require 'telescope.builtin'
